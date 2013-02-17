82b3b1c84c07714376816114d1fc9f5333c87117
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-02-17 15:38:37 +0530

    added travis-ci support

--- test/metrics.js
+++ test/metrics.js
@@ -2,7 +2,7 @@ var assert          = require('assert');
 var metricsIo       = require('../lib/metrics');
 var mongojs         = require('mongojs');
 
-var MONGO_URL = "mongodb://localhost:27017/test-chunk";
+var MONGO_URL = "mongodb://127.0.0.1:27017/test-chunk";
 var COLLECTION = 'metrics';
 
 var db = mongojs(MONGO_URL);
